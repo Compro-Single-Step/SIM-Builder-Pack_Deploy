f7dad1853bf34cdd28ca0ba0c0b84e9ef6f0dc84
gautambishwas <gautam.bishwas@comprotechnologies.com>
2017-04-03 17:21:30 +0530

    Adding Task and Removing Unwanted Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,93 +1,32 @@
 var gulp = require('gulp');
+var path = require('path');
 var git = require('gulp-git');
-var runSequence = require('run-sequence');
+
 var exec = require('child_process').exec;
 var clean = require('gulp-clean');
-var robocopy = require('robocopy');
-var es         = require('event-stream');
-var jeditor = require('gulp-json-editor');
+
 
 gulp.task('clean', function () {
     return gulp.src('checkout', {read: false})
         .pipe(clean());
 });
 
-gulp.task('clone', function(){
-  git.clone('https://tanujaggarwal@github.com/Compro-Single-Step/SIMS-Builder.git',
-            {args:'./checkout/develop'}, function(err) {
-    if (err) throw err;
-  });
-});
-
-gulp.task('checkoutDevelop', function(){
-  git.checkout('SIMPlayer_Preview', {cwd: './checkout/develop'}, function (err) {
-    if (err) throw err;
-  });
-});
-
-gulp.task('cloneRelease', function(){
-  git.clone('https://tanujaggarwal@github.com/Compro-Single-Step/SIMS-Builder.git',
-            {args:'./checkout/qaRelease'}, function(err) {
-    if (err) throw err;
-  });
-});
-
-gulp.task('checkoutRelease', function(){
-  git.checkout('SIM-Builder-Release', {cwd: './checkout/qaRelease'}, function (err) {
-    if (err) throw err;
-  });
-});
-
-var filesToMove = [
-        '/dist',
-        '/package.json'
-    ];
-
-gulp.task('move', function(){
-
-  var distFolder = gulp.src('./checkout/develop/dist/**/*')
-        .pipe(gulp.dest('./checkout/qaRelease/dist'));
-
-  var packageJson = gulp.src('./checkout/develop/package.json')
-      .pipe(jeditor(function(json) {
-        json.scripts.start = "node server/server";
-        return json; // must return JSON object. 
-      }))
-      .pipe(gulp.dest('./checkout/qaRelease/'));
-
-  return es.concat(distFolder, packageJson);
-});
-
-gulp.task('add', function() {
-  return gulp.src('./checkout/qaRelease/*')
-    .pipe(git.add());
-});
-
-gulp.task('commit', function() {
-
-    return gulp.src('./checkout/qaRelease/*')
-      .pipe(git.commit("initial test"));
-
+gulp.task('clone', ['clean'], function(){
+	
+	return my_promise = new Promise( (resolve, reject) => {
+	   git.clone('https://tanujaggarwal@github.com/Compro-Single-Step/SIMS-Builder.git',
+	            {args: path.join(__dirname, 'checkout', 'develop')}, function(err) {
+	    if (err)
+	    	reject(err)
+	    else
+	    	resolve("Success");
+	  });
+  	})
 });
 
 
-gulp.task('push', function(){
-
-  git.push('https://github.com/Compro-Single-Step/SIMS-Builder.git', 'SIM-Builder-Release', function (err) {
-    if (err) throw err;
-  });
-});
-
-gulp.task('gitsend', function() {
-  runSequence('add', 'commit', 'push');
-});
-
-gulp.task('chdir', function(cb) {
-    process.chdir('./checkout/develop');
-    //process.chdir('path');
-});
-
-gulp.task('install', function(cb) {
+gulp.task('install', ['clone'], function(cb) {
+	 process.chdir('./checkout/develop');
     exec('npm install', {maxBuffer: 1024 * 500}, function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
@@ -95,15 +34,30 @@ gulp.task('install', function(cb) {
     });
 });
 
-gulp.task('buildforDeploy', function(cb) {
-    exec('npm build', {maxBuffer: 1024 * 500}, function(err, stdout, stderr) {
+gulp.task('buildforDeploy', ['install'], function(cb) {
+    exec('npm run build', {maxBuffer: 1024 * 500}, function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         cb(err);
     });
 });
 
+gulp.task('move', ['buildforDeploy'], function(){
+	console.log("Moving files in develop folder");
+  	gulp.src('./checkout/develop/dist/**/*')
+        .pipe(gulp.dest('./checkout/qaRelease/dist'));
 
-gulp.task('build', function(callback) {
-  runSequence(['chdir','install','buildforDeploy'],callback);
+	console.log("Moving files in develop folder");
+  	packageJson = gulp.src('./checkout/develop/package.json')
+      .pipe(gulp.dest('./checkout/qaRelease'));
 });
+
+gulp.task('chdirToQA', ['move'], function(cb) {
+    process.chdir('../qaRelease');
+});
+
+//
+//Push Work
+//
+
+gulp.task('default', ['move']);
