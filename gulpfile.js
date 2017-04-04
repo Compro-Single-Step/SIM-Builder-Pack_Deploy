f4ad221c43a9670ac694d4a316084e10773cb2d6
gautambishwas <gautam.bishwas@comprotechnologies.com>
2017-04-04 11:58:52 +0530

    initial test

--- gulpfile.js
+++ gulpfile.js
@@ -63,7 +63,7 @@ gulp.task('chdirToQA', ['move'], function(cb) {
 
 gulp.task('addremote', function(){
 	 //process.chdir('./checkout/qaRelease');
-  git.addRemote('develop7', 'https://github.com/Compro-Single-Step/SIMS-Builder.git', function (err) {
+  git.addRemote('master', 'https://github.com/Compro-Single-Step/SIMS-Builder.git', function (err) {
     if (err) throw err;
   });
 });
@@ -79,7 +79,7 @@ gulp.task('commit', ['addremote','add'], function() {
 });
 
 gulp.task('push', ['commit'], function(){
-  git.push('develop7','SIM-Builder-Release', {args: '-f'}, function (err) {
+  git.push('master','SIM-Builder-Release', {args: '-f'}, function (err) {
     if (err) throw err;
   });
 });
