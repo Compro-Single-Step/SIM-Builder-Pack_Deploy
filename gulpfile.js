fc9c971896e01f416d8a2793a2fa38276cf9e172
gautambishwas <gautam.bishwas@comprotechnologies.com>
2017-04-05 10:44:06 +0530

    Adding New Module

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp');
 var path = require('path');
 var git = require('gulp-git');
-
+var deploy = require('gulp-deploy-git');
 var exec = require('child_process').exec;
 var clean = require('gulp-clean');
+var rename = require('gulp-rename');
+
 
 
 gulp.task('clean', function () {
-    return gulp.src('./checkout/develop', {read: false})
+    return gulp.src('./checkout/', {read: false})
     	
         .pipe(clean());
 });
@@ -25,8 +27,33 @@ gulp.task('clone', ['clean'], function(){
   	})
 });
 
+gulp.task('clone1',['clean'], function(){
+	
+	return my_promise = new Promise( (resolve, reject) => {
+	   git.clone('https://tanujaggarwal@github.com/Compro-Single-Step/SIMS-Builder.git',
+	            {args: path.join(__dirname, 'checkout', 'qaRelease')}, function(err) {
+	    if (err)
+	    	reject(err)
+	    else
+	    	resolve("Success");
+	  });
+  	})
+});
+gulp.task('swtchBranch',['clone1'], function(){
+	return my_promise = new Promise( (resolve, reject) => {
+	  git.checkout('SIM-Builder-Release', {cwd: './checkout/qaRelease',maxBuffer: 1024 * 500},function (err) {
 
-gulp.task('install', ['clone'], function(cb) {
+	    if (err)
+	    	reject(err)
+	    else
+	    	resolve("Success");
+	  });
+  	})
+});
+
+
+
+gulp.task('install', ['clone','swtchBranch'], function(cb) {
 	 process.chdir('./checkout/develop');
     exec('npm install', {maxBuffer: 1024 * 500}, function(err, stdout, stderr) {
         console.log(stdout);
@@ -50,42 +77,59 @@ gulp.task('move', ['buildforDeploy'], function(){
 
 	console.log("Moving files in develop folder");
   	packageJson = gulp.src('./checkout/develop/package.json')
+
       .pipe(gulp.dest('./checkout/qaRelease'));
 });
 
-gulp.task('chdirToQA', ['move'], function(cb) {
+/*gulp.task('chdirToQA', ['move'], function(cb) {
     process.chdir('../qaRelease');
-});
+});*/
 
 //
 //Push Work
 //
 
-gulp.task('addremote', function(){
+/*gulp.task('addremote', function(){
 	 //process.chdir('./checkout/qaRelease');
-  git.addRemote('develop8', 'https://github.com/Compro-Single-Step/SIMS-Builder.git', function (err) {
+  git.addRemote('develop10', 'https://github.com/Compro-Single-Step/SIMS-Builder.git', function (err) {
     if (err) throw err;
   });
 });
 
 gulp.task('add', function() {
+	 
   return gulp.src('./checkout/qaRelease/*')
     .pipe(git.add());
 });
 
-gulp.task('commit', ['addremote','add'], function() {
-    return gulp.src('./*')
+gulp.task('commit', ['add'], function() {
+	 //process.chdir('./checkout/qaRelease');
+    return gulp.src('./checkout/qaRelease/*')
       .pipe(git.commit("initial test"));
 });
 
 gulp.task('push', ['commit'], function(){
-  git.push('develop8','SIM-Builder-Release', {args: '-f'}, function (err) {
-    if (err) throw err;
-  });
+  return my_promise = new Promise( (resolve, reject) => {
+  git.push('origin','SIM-Builder-Release', {refspec: 'HEAD', cwd: './checkout/qaRelease',maxBuffer: 1024 * 500}, function (err) {
+    if (err)
+	    	reject(err)
+	    else
+	    	resolve("Success");
+	  });
+  	})
 });
-gulp.task('merge',['push'], function(){
+*/
+/*gulp.task('merge',['push'], function(){
   git.merge('develop8', function (err) {
     if (err) throw err;
   });
+});*/
+gulp.task('deploy', function() {
+  return gulp.src('./checkout/qaRelease/**/*', { read: false, base:'./checkout'})
+   .pipe(rename({'./checkout/qaRelease': 'qaRelease'}))
+    .pipe(deploy({
+      repository: 'https://github.com/Compro-Single-Step/SIMS-Builder.git',
+      remoteBranch:   'SIM-Builder-Release'
+    }))
 });
 gulp.task('default', ['move']);
